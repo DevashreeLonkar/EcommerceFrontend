dd7d19a508af861dd3120bc96cce3c1baa9ac4bd
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-08-26 23:14:02 +0530

    FrontEnd commit

---
+++ src/app/drag.directive.ts
@@ -0,0 +1,49 @@
+import { style } from '@angular/animations';
+import { Directive, EventEmitter, HostBinding, HostListener, Output, output } from '@angular/core';
+import { FileHandle } from './_model/file-handle.model';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Directive({
+  selector: '[appDrag]'
+})
+export class DragDirective {
+
+  @Output() files: EventEmitter<FileHandle>= new EventEmitter();
+
+  @HostBinding("style.background") private background= "#eee";
+  constructor(private sanitizer: DomSanitizer) { }
+
+  @HostListener("dragover", ["$event"])
+  public onDragOver(evt: DragEvent){
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background= "#999";
+  }
+
+  @HostListener("dragleave", ["$event"])
+  public onDragLeave(evt: DragEvent){
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background= "#eee";
+  }
+
+  @HostListener("drop", ["$event"])
+  public onDrop(evt: DragEvent){
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background= "#eee";
+
+    if (!evt.dataTransfer || evt.dataTransfer.files.length === 0) {
+      return;
+    }
+
+    // let fileHandle: FileHandle= null;
+
+    const file= evt.dataTransfer.files[0];
+    const url= this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(file));
+
+    const fileHandle= {file, url};
+    this.files.emit(fileHandle);
+  }
+
+}
